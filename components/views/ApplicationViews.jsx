ea68fc2e839b828f42cc6c1fafa7ecd6489706b6
Brandon Hickman <brandon_7_77@hotmail.com>
2025-05-02 15:52:42 -0500

    Created shells on modules, created the login/welcome page and added route functionality in NavBar

--- components/views/ApplicationViews.jsx
+++ components/views/ApplicationViews.jsx
@@ -1,6 +1,25 @@
+// PROBABLY NOT GOING TO USE, NO NEED FOR MULTIPLE VIEWS.. USE TERNARY CONDITIONAL RENDERING FOR isAdmin LATER ON..
 
 // Will hold all views and incorporate functionality to determine if user is admin or employee
 
-export const ApplicationViews = () => {
-    return <></>
-} 
+// import { useEffect, useState } from "react"
+// import { AdminViews } from "./AdminViews.jsx"
+// import { EmployeeViews } from "./EmployeeViews.jsx"
+
+
+// export const ApplicationViews = () => {
+//     const [currentUser, setCurrentUser] = useState({})
+
+//     useEffect(() => {
+//         const localShepherdUser = localStorage.getItem("shepherd_user")
+//         const shepherdUserObj = JSON.parse(localShepherdUser)
+
+//         setCurrentUser(shepherdUserObj)
+//     }, [])
+
+//     return currentUser.isAdmin ? (
+//         <AdminViews currentUser={currentUser} />
+//     ) : (
+//         <EmployeeViews currentUser={currentUser} />
+//     )
+// } 
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
+import { EmployeeNav } from "../components/nav/EmployeeNav.jsx";
+//import { OrdersList } from "../components/order/OrderList.jsx";
+import { Welcome } from "../components/welcome/welcome.jsx";
 import "./App.css";
+import { Route, Routes, Outlet } from "react-router-dom"
+import { EmployeeViews } from "../components/views/EmployeeViews.jsx";
+import { Login } from "../components/auth/Login.jsx";
 
-function App() {
-  return <div className="App">Shepherd's Pies</div>;
+// import { Authorized } from "./views/Authorized"
+// import { ApplicationViews } from "./views/ApplicationViews"
+
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    
+      <Route path="*" 
+      element={
+          <EmployeeViews />
+      }
+      />
+    </Routes>
+  )
+
 }
 
-export default App;
+
+// export const App = () => {
+//   return (
+//     <div>
+//     <EmployeeNav />
+//     <Welcome />
+//     </div>
+    
+//   )
+// }
+// <Routes>
+//   <Route path="/login" element={<Login />} />
+
+//   <Route path="*" element={
+//     <Authorized>
+//       <ApplicationViews />
+//       <Application />
+//     </Authorized>
+//   }
+//   />
+// </Routes>
